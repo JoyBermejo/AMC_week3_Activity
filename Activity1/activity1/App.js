69b4b061edfdccbf674603f3d8acd0c556585b06
JoyBermejo <joybermejo719@gmail.com>
2025-01-30 16:34:51 +0800

    TextInput

---
+++ Activity1/activity1/App.js
@@ -0,0 +1,73 @@
+import React from 'react';
+import {StyleSheet, TextInput} from 'react-native';
+import {SafeAreaView, SafeAreaProvider} from 'react-native-safe-area-context';
+
+const TextInputExample = () => {
+  const [name, onChangeName] = React.useState('Name:');
+  const [age, onChangeAge] = React.useState('');
+const [address, onChangeAddress] = React.useState('Address:');
+  const [school, onChangeSchool] = React.useState('');
+const [course, onChangeCourse] = React.useState('Course:');
+  const [email, onChangeEmail] = React.useState('');
+  const [contact, onChangeContact] = React.useState('Contact No.:');
+
+  return (
+    <SafeAreaProvider>
+      <SafeAreaView>
+        <TextInput
+          style={styles.input}
+          onChangeText={onChangeName}
+          value={name}
+        />
+        <TextInput
+          style={styles.input}
+          onChangeText={onChangeAge}
+          value={age}
+          placeholder="Age:"
+          keyboardType="numeric"
+        />
+        <TextInput
+          style={styles.input}
+          onChangeText={onChangeAddress}
+          value={address}
+          />
+        <TextInput
+          style={styles.input}
+          onChangeText={onChangeSchool}
+          value={school}
+          placeholder="School"
+          keyboardType="numeric"
+        />
+        <TextInput
+          style={styles.input}
+          onChangeText={onChangeCourse}
+          value={course}
+          />
+        <TextInput
+          style={styles.input}
+          onChangeText={onChangeEmail}
+          value={email}
+          placeholder="Email:"
+          keyboardType="numeric"
+        />
+        <TextInput
+          style={styles.input}
+          onChangeText={onChangeContact}
+          value={contact}
+          />
+      </SafeAreaView>
+    </SafeAreaProvider>
+  );
+};
+
+const styles = StyleSheet.create({
+  input: {
+    height: 40,
+    margin: 12,
+    borderWidth: 1,
+    padding: 10,
+  },
+});
+
+export default TextInputExample;
+
